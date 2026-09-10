3e3c5f5360fdc75be38d060008d7140dcf8357a6
Benjamín Espinoza (C.E.M) <nocturnalboulevardsuite@gmail.com>
2026-09-10 15:31:41 -0300

    Update trendhype.py

--- APP/pages/trendhype.py
+++ APP/pages/trendhype.py
@@ -197,9 +197,8 @@ st.markdown(f"""
     .filter-label-text {{
         color: {text_color} !important;
         font-weight: 700 !important;
-        font-size: 0.95rem !important;
+        font-size: 0.69rem !important;
         margin: 0 !important;
-        white-space: nowrap;
         display: flex;
         align-items: center !important;
         justify-content: flex-start;
